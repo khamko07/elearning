f7efac94ff395571f0f87b2b8493f820562c5dc7
khamko07 <khamko@pascaliaasia.com>
2025-10-26 17:12:17 +0700

    addd

--- admin/navigation/navigations.php
+++ admin/navigation/navigations.php
@@ -75,9 +75,6 @@
                 </li> -->
 
                 <li>
-                     <a href="<?php echo web_root; ?>admin/modules/lesson/index.php"><i class="fa fa-user fa-fw"></i> Lesson </a> 
-                </li>
-                <li>
                      <a href="<?php echo web_root; ?>admin/modules/content/index.php"><i class="fa fa-file-text-o fa-fw"></i> Content </a> 
                 </li>
                  <li>
